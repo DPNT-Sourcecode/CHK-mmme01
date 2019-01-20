d77764379c5b631a977117ef4c076e35f39580a6
sbx_user1060 <sbx_user1060@ip-10-129-205-145.eu-west-2.compute.internal>
2019-01-20 16:09:07 +0000

    Sun Jan 20 16:09:07 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,34 @@
 
 
+VALID_ENTRIES = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    Im presuming that skus is going to look something like this
+
+    'ABCDABBCA'
+
+    :param skus:
+    :return:
+    """
+    items_count = {}
+    skus = skus.upper()
+
+
+    for item in skus:
+        if not validate_entry(item):
+            print('%s is not a valid entry' % item)
+            return -1
+
+        if item in items_count:
+
+
+def validate_entry(item):
+    return item in VALID_ENTRIES.keys()
